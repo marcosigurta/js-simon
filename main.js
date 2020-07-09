5539855e68c181ff58db83d79cb9fb987e228c09
marco <marcosigurta.ovb@gmail.com>
2020-07-09 16:52:42 +0200

    Debug

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
-// Un alert espone 5 numeri casuali (univoci).
-// Poi parte un timer di 30 secondi.
-// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
-// Dividete in piccoli problemi la consegna.
-// Individuate gli elementi di cui avete bisogno.
-// Usate inizialmente prompt e  console.log() , tanti console.log(), se volete aggiungere effetti particolari al DOM fatelo solo in un secondo momento e in cartella bonus :wink:
-
 var numAlert = [];
 var arrayNumutente = [];
 var numeriesatti = [];
 randomAlert();
 setTimeout(userNum, 1000);
 
-//funzione numeri casuali
 function getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso 
 }
-
-//creare funzione che crea 5 numeri random
 
 function randomAlert() {
     for (var i = 0; i < 5; i++) {
@@ -30,15 +19,12 @@ function randomAlert() {
     console.log("numerialert: " + numAlert);
 }
 
-//creo funzione che dopo 30 secondi dalla scomparsa dell'alert faccia comparire 4 prompt
-
 function userNum() {
     for (var i = 0; i < 5; i++) {
         var inputNum = parseInt(prompt('Numero'));
         arrayNumutente.push(inputNum);
     }
-    console.log(arrayNumutente);
-    //creo un controllo sugli elementi delle due 
+    console.log("Numeri scelti dall'utente: " + arrayNumutente);
     for (var i = 0; i < numAlert.length; i++) {
         for (var j = 0; j < arrayNumutente.length; j++) {
             if (numAlert[i] == arrayNumutente[j]) {
